532c1c8d9b2d6614b94fb47749b5b1078429a5f2
CIAC <CIAC@CIAC2-021.academic.mixcoac.upmx.mx>
2023-10-18 16:43:55 -0600

    Update

--- src/components/Projects/ProjectsAlberto/ProjectsAlberto.jsx
+++ src/components/Projects/ProjectsAlberto/ProjectsAlberto.jsx
@@ -9,37 +9,33 @@ function ProjectsAlberto() {
     <div className='projectsAlbertoContent'>
         <h1>Projects Alberto</h1>
         <div className='projectsGrid'>
-            <div className='leftColumn'>
-                <div className='projectInfo'>
-                    <h4>
-                        RayTracer <br/>
-                        <h6><a href='https://github.com/albertomoralesv/RayTracer-en-Java' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/RayTracer-en-Java</a></h6>
-                    </h4>
-                    <img className='projectImage' src={raytracerImage} alt="RayTracer Project" />
-                </div>
-                <div className='projectInfo'>
-                    <h4>
-                        Calculator <br/>
-                        <h6><a href='https://github.com/albertomoralesv/Calculadora---Android' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/Calculadora---Android</a></h6>
-                    </h4>
-                    <img className='projectImage' src={calculatorImage} alt="Calculator Project" />
-                </div>
+            <div className='projectInfo'>
+                <h4>
+                    RayTracer
+                </h4>
+                <h6><a href='https://github.com/albertomoralesv/RayTracer-en-Java' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/RayTracer-en-Java</a></h6>
+                <img className='projectImage' src={raytracerImage} alt="RayTracer Project" />
             </div>
-            <div className='rightColumn'>
-                <div className='projectInfo'>
-                    <h4>
-                        MapOverlay <br/>
-                        <h6><a href='https://github.com/albertomoralesv/MapOverlay' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/MapOverlay</a></h6>
-                    </h4>
-                    <img className='projectImage' src={mapOverlayImage} alt="MapOverlay Project" />
-                </div>
-                <div className='projectInfo'>
-                    <h4>
-                        Lluvia de Palabras <br/>
-                        <h6><a href='https://github.com/albertomoralesv/Juego-en-C' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/Juego-en-C</a></h6>
-                    </h4>
-                    <img className='projectImage' src={cGameImage} alt="Game in C Project" />
-                </div>
+            <div className='projectInfo'>
+                <h4>
+                    Calculator <br/>
+                </h4>
+                <h6><a href='https://github.com/albertomoralesv/Calculadora---Android' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/Calculadora---Android</a></h6>
+                <img className='projectImage' src={calculatorImage} alt="Calculator Project" />
+            </div>
+            <div className='projectInfo'>
+                <h4>
+                    MapOverlay <br/>
+                </h4>
+                <h6><a href='https://github.com/albertomoralesv/MapOverlay' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/MapOverlay</a></h6>
+                <img className='projectImage' src={mapOverlayImage} alt="MapOverlay Project" />
+            </div>
+            <div className='projectInfo'>
+                <h4>
+                    Lluvia de Palabras <br/>
+                </h4>
+                <h6><a href='https://github.com/albertomoralesv/Juego-en-C' target='_blank' rel="noreferrer">https://github.com/albertomoralesv/Juego-en-C</a></h6>
+                <img className='projectImage' src={cGameImage} alt="Game in C Project" />
             </div>
         </div>
     </div>
