b8874762ebb02dd694464f01e0d045c195d565eb
CodeWaffl3 <0218762@up.edu.mx>
2023-10-17 20:11:51 -0600

    Revert "Merge pull request #1 from CodeWaffl3/alberto"
    This reverts commit d9640b75ad74333f611e89502a7033c39ba6fc29, reversing
    changes made to 27d51889ef462683dbd18d62da905d493c5194d0.

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,31 @@
-import { useState } from 'react';
 import './App.css'
-import Navbar from './components/Navbar/Navbar';
-import SliderHeader from "./components/SliderHeader/SliderHeader";
-import AboutMe from './components/AboutMe/AboutMe'
-import Resume from './components/Resume/Resume';
-import Projects from './components/Projects/Projects';
-import SocialMedia from './components/SocialMedia/SocialMedia';
-import Gallery from './components/Gallery/Gallery';
+import SliderHeader from "./components/SliderHeader/SliderHeader.jsx";
+import {useState} from "react";
+
 
 function App() {
-  const [selectedPage, setselectedPage] = useState('Home');
-  const [userShown, setUserShown] = useState('');
+    const [scroll, setScroll] = useState(false);
 
-  const handleNavClick = (content) => {
-    setselectedPage(content);
-  };
+    const handleClick = () =>{
+        setScroll(!scroll);
+        if (scroll)
+        {
+            document.body.style.overflow = "hidden"
 
-  const handleUserClick = (user) => {
-    setUserShown(user);
-  };
+        }
+        else if (!scroll)
+        {
+            document.body.style.overflow = "visible"
+        }
+    }
 
-  return (
+
+    return (
     <>
-    <div className='app'>
-      <Navbar onNavClick={handleNavClick} onUserClick={handleUserClick}>
-
-      </Navbar>
-      <div className='mainContent'>
-        {selectedPage === 'Home' && (
-          <SliderHeader>
-
-          </SliderHeader>
-        )}
-        {selectedPage === 'AboutMe' && (
-          <AboutMe activeUser={userShown}>
-            
-          </AboutMe>
-        )}
-        {selectedPage === 'Resume' && (
-          <Resume activeUser={userShown}>
-
-          </Resume>
-        )}
-        {selectedPage === 'Projects' && (
-          <Projects activeUser={userShown}>
-
-          </Projects>
-        )}
-        {selectedPage === 'SocialMedia' && (
-          <SocialMedia activeUser={userShown}>
-
-          </SocialMedia>
-        )}
-        {selectedPage === 'Gallery' && (
-          <Gallery activeUser={userShown}>
-            
-          </Gallery>
-        )}
-      </div>
-    </div>
+        <button onClick={handleClick}>CLICK ME</button>
+        <SliderHeader onClick={handleClick}/>
     </>
-  )
+    )
 }
 
 export default App
