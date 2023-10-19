6c0405cd3a1b80cf50ea0a590f3e04f3311aef4b
CodeWaffl3 <0218762@up.edu.mx>
2023-10-19 17:26:12 -0600

    Socials

--- src/components/SocialMedia/SocialMediaGabriel/SocialMediaGabriel.jsx
+++ src/components/SocialMedia/SocialMediaGabriel/SocialMediaGabriel.jsx
@@ -10,18 +10,25 @@ export default function SocialMediaGabriel() {
         <div className='socialMediaGabriel'>
             <h1 className="titleSocialMedia">Connect with Gabriel</h1>
             <div className="socials">
-                <div className="socialContainer">
-                    <h3>Github</h3>
-                    <img className="imagesSocial" src={githubLogo} alt="github" />
-                </div>
-                <div className="socialContainer">
-                    <h3>Gmail</h3>
-                    <img width={130} src={gmailLogo} alt="github" />
-                </div>
-                <div className="socialContainer">
-                    <h3>LinkedIn</h3>
-                    <img width={130} src={linkedinLogo} alt="github" />
-                </div>
+                <a href="https://github.com/CodeWaffl3">
+                    <div className="socialContainer">
+                        <h3>Github</h3>
+                        <img className="imagesSocial" src={githubLogo} alt="github" />
+                    </div>
+                </a>
+                <a href="mailto:0218762@up.edu.mx">
+                    <div className="socialContainer">
+                        <h3>Gmail</h3>
+                        <img width={130} src={gmailLogo} alt="github" />
+                    </div>
+                </a>
+                <a href="https://www.linkedin.com/in/gabriel-andrés-cárdenas-tapia-7b8548259">
+                    <div className="socialContainer">
+                        <h3>LinkedIn</h3>
+                        <img width={130} src={linkedinLogo} alt="github" />
+                    </div>
+                </a>
+                
             </div>
             
         </div>
